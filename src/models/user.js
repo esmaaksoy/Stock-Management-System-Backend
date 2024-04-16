1a29d2ad617fe3b8c5e0fe814adc8b00d2f481f1
esma <esma.ea.aksoy@gmail.com>
2024-04-16 15:45:17 +0300

    feat: user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,71 @@
-"use strict"
+"use strict";
 
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "User name field must be required"],
+      unique: [true, "User name field must be unique"],
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Email field must be required"],
+      unique: [true, "Email field must be unique"],
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Email type is not correct.",
+      ],
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: [true, "Password field must be required"],
+      set: (password) => {
+        if (
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)
+        ) {
+          return passwordEncrypt(password);
+        } else {
+          throw new Error("Password type is not correct.");
+        }
+      },
+    },
+
+    firstName: {
+      type: String,
+      trim: true,
+      required: [true, "First name field must be required"],
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+      required: [true, "Last name field must be required"],
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { collection: "users", timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
