2a8a20f7ac4bd3200d5386c8895e944ecdafad84
esma <esma.ea.aksoy@gmail.com>
2024-04-16 18:23:28 +0300

    feat: user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,8 @@
 "use strict";
 
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 const User = require("../models/user");
+const Token = require("../models/token");
 
 module.exports = {
   list: async (req, res) => {
@@ -18,6 +20,7 @@ module.exports = {
             `
         */
 
+    //! Admin değilse sadece kendi kaydını listeleyebilir. Normalde permissionlar ile bunu route da yapıyorum ama burada da bu şekilde yapabilirim.
     const customFilters = req.user?.isAdmin ? {} : { _id: req.user._id };
     const data = await User.getModelList(User, customFilters);
 
@@ -46,14 +49,95 @@ module.exports = {
 
     req.body.isStaff = false;
     req.body.isAdmin = false;
-    const data = await User.create(req.body)
+    const data = await User.create(req.body);
+
+    // Auto Login
+    const tokenData = await Token.create({
+      userId: data._id,
+      token: passwordEncrypt(data._id + Date.now()),
+    });
 
     res.status(201).send({
-        error:false,
-        data
-    })
+      error: false,
+      token: tokenData.token,
+      data,
+    });
   },
-//   read: async (req, res) => {},
-//   update: async (req, res) => {},
-//   delete: async (req, res) => {},
+  read: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
+        */
+
+    const customFilters = req.user?.isAdmin
+      ? { _id: req.params.id }
+      : { _id: req.user._id };
+
+    const data = await User.findOne(customFilters);
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
+    const customFilters = req.user?.isAdmin
+      ? { _id: req.params.id }
+      : { _id: req.user._id };
+
+    //! Yeni kayıtlarda admin/staff durumu değiştirilemez:
+    if (!req.user?.isAdmin) {
+      delete req.body.isStaff;
+      delete req.body.isAdmin;
+    }
+
+    const data = await User.updateOne(customFilters, req.body, {
+      runValidators: true,
+    });
+
+    res.status(202).send({
+      error: false,
+      data,
+      new: await User.findOne(customFilters),
+    });
+  },
+
+  delete: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
+    //! Kullanıcı kendi kaydını silemesin diye yaptım. Sadece admin silebilsin kaydını permissions ile yaptım.
+
+    if (req.params.id != req.user._id) {
+
+      const data = await User.deleteOne({ _id: req.params.id })
+
+      res.status(data.deletedCount ? 204 : 404).send({
+          error: !data.deletedCount,
+          data
+      })
+
+  } else {
+      // Admin kendini silemez.
+      res.errorStatusCode = 403
+      throw new Error('You can not remove your account.')
+  }
+
+  },
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,9 @@
-"use strict"
+"use strict";
 
-const router = require('express').Router()
-const user = require("../controllers/user")
+const router = require("express").Router();
+const user = require("../controllers/user");
 
-router.route("/").get(user.list).post(user.create)
+router.route("/").get(user.list).post(user.create);
 
-module.exports = router
+router.route("/:id").put(user.update);
+module.exports = router;
