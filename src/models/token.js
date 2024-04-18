c7aa3facf0a34ff55aa7f88115e47b15dae5df09
esma <esma.ea.aksoy@gmail.com>
2024-04-18 13:18:25 +0300

    feat: created token

--- src/models/token.js
+++ src/models/token.js
@@ -1,3 +1,24 @@
 "use strict"
 
 const { mongoose } = require('../configs/dbConnection')
+
+const TokenSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true,
+    }, 
+
+    token: {
+        type: String,
+        trim: true,
+        required: true,
+        index: true,
+    }, 
+
+}, { collection: 'tokens', timestamps: true })
+
+
+module.exports = mongoose.model('Token', TokenSchema)
--- src/routes/token.js
+++ src/routes/token.js
@@ -1,5 +1,19 @@
-"use strict"
+"use strict";
 
-const router = require('express').Router()
+const router = require("express").Router();
 
-module.exports = router
+const { isAdmin } = require("../middlewares/permissions");
+const token = require("../controllers/token");
+
+router.use(isAdmin);
+
+router.route("/").get(token.list).post(token.create);
+
+router
+  .route("/:id")
+  .get(token.read)
+  .put(token.update)
+  .patch(token.update)
+  .delete(token.delete);
+  
+module.exports = router;
