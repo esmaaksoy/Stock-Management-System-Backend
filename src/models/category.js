69d548396fe41300bcbd7bf4bc498866b84fa95a
esma <esma.ea.aksoy@gmail.com>
2024-04-18 13:39:19 +0300

    feat: created category

--- src/models/category.js
+++ src/models/category.js
@@ -1,3 +1,21 @@
-"use strict"
+"use strict";
 
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      set: function (name) {
+        //! girilen categorilerin ilk harfini büyük yaptım.
+        return name.charAt(0).toUpperCase() + name.slice(1);
+      },
+    },
+  },
+  { collection: "categories", timestamps: true }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,5 +1,18 @@
-"use strict"
+"use strict";
 
-const router = require('express').Router()
+const router = require("express").Router();
+const category = require("../controllers/category");
+const permissions = require("../middlewares/permissions");
 
-module.exports = router
+router
+  .route("/")
+  .get(permissions.isStaff, category.list)
+  .post(permissions.isStaff, category.create);
+
+router.route('/:id')
+    .get(permissions.isStaff, category.read)
+    .put(permissions.isAdmin, category.update)
+    .patch(permissions.isAdmin, category.update)
+    .delete(permissions.isAdmin, category.delete)
+
+module.exports = router;
