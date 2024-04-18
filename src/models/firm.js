b58c334027b6236a258bf7d68968339a774a056c
esma <esma.ea.aksoy@gmail.com>
2024-04-18 14:01:37 +0300

    feat: creted firm

--- src/models/firm.js
+++ src/models/firm.js
@@ -1,3 +1,39 @@
-"use strict"
+"use strict";
 
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+
+
+const FirmSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    phone: String,
+
+    address: String,
+
+    images: [
+      {
+        type: String,
+        trim: true,
+        validate: {
+            validator: function(img) {
+              return /(https?:\/\/.*\.(?:png|jpg))/i.test(img);
+            },
+            message: props => `${props.value} is not a valid image url!`
+          },
+      },
+    ],
+  },
+  {
+    collection: "firms",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Firm", FirmSchema);
+
--- src/routes/firm.js
+++ src/routes/firm.js
@@ -1,5 +1,20 @@
-"use strict"
+"use strict";
 
-const router = require('express').Router()
+const router = require("express").Router();
 
-module.exports = router
+const firm = require("../controllers/firm");
+const permissions = require("../middlewares/permissions");
+
+router
+  .route("/")
+  .get(permissions.isStaff, firm.list)
+  .post(permissions.isAdmin, firm.create);
+
+router
+  .route("/:id")
+  .get(permissions.isStaff, firm.read)
+  .put(permissions.isAdmin, firm.update)
+  .patch(permissions.isAdmin, firm.update)
+  .delete(permissions.isAdmin, firm.delete);
+
+module.exports = router;
