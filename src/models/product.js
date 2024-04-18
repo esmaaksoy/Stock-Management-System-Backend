6d5dba5ad666e4dbd3d065e471da43db4c676e2a
esma <esma.ea.aksoy@gmail.com>
2024-04-18 14:10:49 +0300

    feat: created product

--- src/models/product.js
+++ src/models/product.js
@@ -1,3 +1,41 @@
-"use strict"
+"use strict";
 
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+
+    brandId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true,
+    },
+
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      default: 0,
+    },
+
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    collection: "products",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,5 +1,20 @@
-"use strict"
+"use strict";
 
-const router = require('express').Router()
+const router = require("express").Router();
 
-module.exports = router
+const product = require("../controllers/product");
+const permissions = require("../middlewares/permissions");
+
+router
+  .route("/")
+  .get(permissions.isStaff, product.list)
+  .post(permissions.isAdmin, product.create);
+
+router
+  .route("/:id")
+  .get(permissions.isStaff, product.read)
+  .put(permissions.isAdmin, product.update)
+  .patch(permissions.isAdmin, product.update)
+  .delete(permissions.isAdmin, product.delete);
+
+module.exports = router;
