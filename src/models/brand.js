067f7976eca220a5c11418568f9e0baed78a242c
esma <esma.ea.aksoy@gmail.com>
2024-04-18 13:44:01 +0300

    feat: created brand

--- src/models/brand.js
+++ src/models/brand.js
@@ -1,3 +1,25 @@
-"use strict"
+"use strict";
 
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+
+const BrandSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    image: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    collection: "brands",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Brand", BrandSchema);
--- src/routes/brand.js
+++ src/routes/brand.js
@@ -1,5 +1,19 @@
-"use strict"
+"use strict";
 
-const router = require('express').Router()
+const router = require("express").Router();
+const brand = require("../controllers/brand");
+const permissions = require("../middlewares/permissions");
 
-module.exports = router
+router
+  .route("/")
+  .get(permissions.isStaff, brand.list)
+  .post(permissions.isAdmin, brand.create);
+
+router
+  .route("/:id")
+  .get(permissions.isStaff, brand.read)
+  .put(permissions.isAdmin, brand.update)
+  .patch(permissions.isAdmin, brand.update)
+  .delete(permissions.isAdmin, brand.delete);
+
+module.exports = router;
