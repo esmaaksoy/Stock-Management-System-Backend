5a051769306ae1462010f91eb4194b3dcf4b40d6
esma <esma.ea.aksoy@gmail.com>
2024-04-18 21:42:05 +0300

    bug: fixed logger file

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/upload', express.static('./upload'))
 app.use(require('./src/middlewares/authentication'))
 
 // Run Logger:
-app.use(require('./src/middlewares/logger'))
+// app.use(require('./src/middlewares/logger'))
 
 // res.getModelList():
 app.use(require('./src/middlewares/queryHandler'))
